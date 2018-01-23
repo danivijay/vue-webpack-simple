9b1e7a1ab36d360e18692f68644846b2d7df34aa
Dani Vijay <danimvijay@gmail.com>
2018-01-23 12:44:51 +0530

    project init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greetMe() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Welcome to Vue'
+    }
+  },
+  methods: {
+    greetMe () {
+      return 'this is some Vueeeee!!'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
